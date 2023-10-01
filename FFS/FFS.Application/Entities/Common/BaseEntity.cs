60021bb0c8053da0b22c6601fe0507c7e4fbb57a
DucHieu <daoduchieu171221@gmail.com>
2023-10-01 19:46:13 +0700

    init

---
+++ FFS/FFS.Application/Entities/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FFS.Application.Entities.Common
+{
+    public class BaseEntity<T>
+    {
+        [Key]
+        public virtual T Id { get;set; }
+        public virtual DateTime CreatedAt { get; set; }
+        public virtual DateTime UpdatedAt { get; set; }
+    }
+}
