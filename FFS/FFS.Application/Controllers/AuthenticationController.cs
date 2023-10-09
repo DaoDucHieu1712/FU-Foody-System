a7357ee7c9e4acd2a0eff0d53ea4e7e11e99f530
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2023-10-09 17:37:30 +0700

    Vinhlq_09/10/2023_1
    Update authen controller api

---
+++ FFS/FFS.Application/Controllers/AuthenticationController.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+using AutoMapper;
+
+using FFS.Application.Data;
+using FFS.Application.DTOs;
+using FFS.Application.Entities;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FFS.Application.Controllers {
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public AuthenticateController(ApplicationDbContext db, UserManager<ApplicationUser> userManager) {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterShipper(ShipperRegisterDTO shipperRegisterDTO)
+        {
+            try
+            {
+                string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+                Regex regex = new Regex(pattern);
+
+                if (!regex.IsMatch(shipperRegisterDTO.email))
+                {
+                    throw new Exception("Email không hợp lệ!");
+                }
+                //_userManager.CreateAsync(shipperRegisterDTO, shipperRegisterDTO.password);
+                return Ok();
+           
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
