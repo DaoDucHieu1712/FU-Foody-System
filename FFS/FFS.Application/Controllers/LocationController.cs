9d60e99aa2915219f277f94310ca7c4ca3804e84
Tungo01 <79755823+Tungo01@users.noreply.github.com>
2023-10-10 01:14:04 +0700

    TungNT_10/10/2023_1
    CRUD API Location + FE form

---
+++ FFS/FFS.Application/Controllers/LocationController.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using FFS.Application.Data;
+using FFS.Application.DTOs;
+using FFS.Application.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FFS.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public LocationController(ApplicationDbContext db) {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Location>>> ListLocation()
+        {
+            try
+            {
+                var locations = await _db.Locations.ToListAsync();
+                return Ok(locations);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Location>> AddLocation(Location location)
+        {
+            try
+            {
+                _db.Locations.Add(location);
+                await _db.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateLocation(int id, Location location)
+        {
+            try
+            {
+                if (id != location.Id)
+                {
+                    return BadRequest();
+                }
+                _db.Entry(location).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLocation(int id)
+        {
+            try
+            {
+                var location = await _db.Locations.FindAsync(id);
+                if (location == null)
+                {
+                    return NotFound();
+                }
+                _db.Locations.Remove(location);
+                await _db.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ FFS/FFS.Application/DTOs/LocationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FFS.Application.DTOs
+{
+    public class LocationDTO
+    {
+        public string UserId { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
+        public virtual DateTime UpdatedAt { get; set; }
+        public virtual bool IsDelete { get; set; }
+        public string Address { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
