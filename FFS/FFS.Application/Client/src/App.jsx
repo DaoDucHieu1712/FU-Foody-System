3c0985bee7eb42ec3f3210890700748614c8baac
DucHieu <daoduchieu171221@gmail.com>
2023-10-09 17:37:08 +0700

    layout

--- FFS/FFS.Application/Client/src/App.jsx
+++ FFS/FFS.Application/Client/src/App.jsx
@@ -1,7 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<>Home Page</>} />
+      </Routes>
     </>
   );
 }
