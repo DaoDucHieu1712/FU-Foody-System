8a92b1a530b34b299b3ff4743eede4557fb07151
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2023-10-09 22:11:05 +0700

    VinhLQ_09/10/2023_2
    Update register with shipper

--- FFS/FFS.Application/Controllers/AuthenticationController.cs
+++ FFS/FFS.Application/Controllers/AuthenticationController.cs
@@ -6,10 +6,13 @@ using AutoMapper;
 using FFS.Application.Data;
 using FFS.Application.DTOs;
 using FFS.Application.Entities;
+using FFS.Application.Entities.Constant;
+using FFS.Application.Helper;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FFS.Application.Controllers {
     [Route("api/[controller]/[action]")]
@@ -19,7 +22,8 @@ namespace FFS.Application.Controllers {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IMapper _mapper;
 
-        public AuthenticateController(ApplicationDbContext db, UserManager<ApplicationUser> userManager) {
+        public AuthenticateController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
             _db = db;
             _userManager = userManager;
         }
@@ -27,23 +31,130 @@ namespace FFS.Application.Controllers {
         [HttpPost]
         public async Task<IActionResult> RegisterShipper(ShipperRegisterDTO shipperRegisterDTO)
         {
+            using var transaction = await _db.Database.BeginTransactionAsync();
             try
             {
                 string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-
                 Regex regex = new Regex(pattern);
-
                 if (!regex.IsMatch(shipperRegisterDTO.email))
                 {
                     throw new Exception("Email không hợp lệ!");
                 }
-                //_userManager.CreateAsync(shipperRegisterDTO, shipperRegisterDTO.password);
-                return Ok();
-           
+
+                ApplicationUser user = await _userManager.FindByEmailAsync(shipperRegisterDTO.email);
+                if (user != null)
+                {
+                    throw new Exception("Email đã tồn tại! Xin vui lòng thử lại");
+                }
+
+                if (!ValidatePassword.IsStrongPassword(shipperRegisterDTO.password))
+                {
+                    throw new Exception("Mật khẩu phải nhiều hơn 8 kí tư, có chữ in hoa, chữ thường, số và kí tự đặc biệt!");
+                }
+                var shipper = new ApplicationUser()
+                {
+                    Email = shipperRegisterDTO.email,
+                    UserName = ExtractUsername(shipperRegisterDTO.email)
+                };
+
+                IdentityResult check = await _userManager.CreateAsync(shipper, shipperRegisterDTO.password);
+                if (check.Succeeded == false)
+                {
+                    var specificErrors = check.Errors.FirstOrDefault();
+                    throw new Exception(specificErrors?.Description);
+                }
+                IdentityRole? role = await _db.Roles.FirstOrDefaultAsync(role => role.NormalizedName == Role.SHIPPER);
+                if (role == null)
+                {
+                    throw new Exception("Có lỗi xảy ra vui lòng liên hệ Admin!");
+                }
+                IdentityUserRole<string> userRole = new IdentityUserRole<string>
+                {
+                    UserId = shipper.Id,
+                    RoleId = role.Id
+                };
+                await _db.UserRoles.AddAsync(userRole);
+                await _db.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return Ok("Đăng kí thành công tài khoản shipper");
+
             }
             catch (Exception ex)
             {
+                await transaction.RollbackAsync();
                 return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(ShipperRegisterDTO shipperRegisterDTO)
+        {
+            using var transaction = await _db.Database.BeginTransactionAsync();
+            try
+            {
+                string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+                Regex regex = new Regex(pattern);
+                if (!regex.IsMatch(shipperRegisterDTO.email))
+                {
+                    throw new Exception("Email không hợp lệ!");
+                }
+
+                ApplicationUser user = await _userManager.FindByEmailAsync(shipperRegisterDTO.email);
+                if (user != null)
+                {
+                    throw new Exception("Email đã tồn tại! Xin vui lòng thử lại");
+                }
+
+                if (!ValidatePassword.IsStrongPassword(shipperRegisterDTO.password))
+                {
+                    throw new Exception("Mật khẩu phải nhiều hơn 8 kí tư, có chữ in hoa, chữ thường, số và kí tự đặc biệt!");
+                }
+                var shipper = new ApplicationUser()
+                {
+                    Email = shipperRegisterDTO.email,
+                    UserName = ExtractUsername(shipperRegisterDTO.email)
+                };
+
+                IdentityResult check = await _userManager.CreateAsync(shipper, shipperRegisterDTO.password);
+                if (check.Succeeded == false)
+                {
+                    var specificErrors = check.Errors.FirstOrDefault();
+                    throw new Exception(specificErrors?.Description);
+                }
+                IdentityRole? role = await _db.Roles.FirstOrDefaultAsync(role => role.NormalizedName == Role.SHIPPER);
+                if (role == null)
+                {
+                    throw new Exception("Có lỗi xảy ra vui lòng liên hệ Admin!");
+                }
+                IdentityUserRole<string> userRole = new IdentityUserRole<string>
+                {
+                    UserId = shipper.Id,
+                    RoleId = role.Id
+                };
+                await _db.UserRoles.AddAsync(userRole);
+                await _db.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return Ok("Đăng kí thành công tài khoản shipper");
+
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        string ExtractUsername(string emailAddress)
+        {
+            int atIndex = emailAddress.IndexOf("@");
+
+            if (atIndex != -1)
+            {
+                return emailAddress.Substring(0, atIndex);
+            }
+            else
+            {
+                return emailAddress;
             }
         }
     }
---
+++ FFS/FFS.Application/Entities/Constant/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace FFS.Application.Entities.Constant {
+    public static class Role {
+        public const string USER = "USER";
+        public const string SHIPPER = "SHIPPER";
+        public const string STOREOWNER = "STOREOWNER";
+        public const string ADMIN = "ADMIN";
+
+    }
+}
