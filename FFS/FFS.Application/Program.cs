77f963a3cc30eafd4074314c2fd9990d63c85628
TuanNguyen2204 <tuanht22042001@gmail.com>
2023-10-15 02:01:20 +0700

    add report dbset and generate jwt token

--- FFS/FFS.Application/Program.cs
+++ FFS/FFS.Application/Program.cs
@@ -9,6 +9,7 @@ using FFS.Application.Repositories.Impls;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -41,6 +42,9 @@ builder.Services.Configure<EmailSettingModel>(configuration.GetSection("MailSett
 #endregion
 
 #region authentication
+builder.Services.Configure<AppSetting>(builder.Configuration.GetSection("JWT"));
+builder.Services.Configure<EmailSettingModel>(builder.Configuration.GetSection("MailSettings"));
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -79,7 +83,6 @@ builder.Services.AddTransient(typeof(IRepository<,>), typeof(EntityRepository<,>
 builder.Services.AddTransient<IAuthRepository, AuthRepository>();
 builder.Services.AddTransient<ITokenRepository, TokenRepository>();
 builder.Services.AddTransient<IEmailService, EmailService>();
-builder.Services.AddScoped<AppSetting>();
 #endregion
 
 #region swagger
@@ -123,6 +126,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+
 
 app.UseHttpsRedirection();
 app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
