e18ff4f40e171556cecd8883fa1a746fc7948354
TuanNguyen2204 <tuanht22042001@gmail.com>
2023-11-04 22:21:10 +0700

    TuanNV_4/11/2023_v1

--- FFS/FFS.Application/DTOs/Food/FoodRatingDTO.cs
+++ FFS/FFS.Application/DTOs/Food/FoodRatingDTO.cs
@@ -4,17 +4,10 @@ namespace FFS.Application.DTOs.Food
 {
     public class FoodRatingDTO
     {
-        public string? UserId { get; set; }
-        public List<FoodRatingItem>? FoodRatings { get; set; }
-        public string? ShipperId { get; set; }
-        public string? NoteForShipper { get; set; }
-        public string? Content { get; set; }
-        public ICollection<ImageFoodRatingDTO>? Images { get; set; }
-    }
-
-    public class FoodRatingItem
-    {
+        public string? Email { get; set; }
         public int? FoodId { get; set; }
         public int? Rate { get; set; }
+        public string? Content { get; set; }
+        public ICollection<ImageFoodRatingDTO>? Images { get; set; }
     }
 }
