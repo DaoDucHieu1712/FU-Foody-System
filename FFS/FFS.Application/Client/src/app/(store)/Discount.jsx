18d234a7c30464d370ce9205e3ba343e0e20b64a
Tungo01 <79755823+Tungo01@users.noreply.github.com>
2023-11-11 16:43:03 +0700

    Tungnt_11112023_v2
    Fix bug

--- FFS/FFS.Application/Client/src/app/(store)/Discount.jsx
+++ FFS/FFS.Application/Client/src/app/(store)/Discount.jsx
@@ -16,9 +16,7 @@ import AddDiscount from "./components/Discount/AddDiscount";
 import UpdateDiscount from "./components/Discount/UpdateDiscount";
 import DeleteDiscount from "./components/Discount/DeleteDiscount";
 
-
-const TABLE_HEAD = ["Mã ưu đãi", "Phần trăm giảm giá (%)", "Áp dụng với đơn hàng (nghìn VND)", "Số lượng", "Ngày hết hạn", ""];
-
+const TABLE_HEAD = ["Id", "Mã ưu đãi", "Phần trăm giảm giá (%)", "Áp dụng với đơn hàng (nghìn VND)", "Số lượng", "Ngày hết hạn", "Tình trạng", ""];
 
 const Discount = () => {
   const backgroundColors = ["bg-gray-50", "bg-gray-200"];
@@ -31,45 +29,28 @@ const Discount = () => {
   const uId = CookieService.getToken("fu_foody_id");
 
 
-    const GetStoreByUid = async () => {
-        try {
-            await axios
-                .get(`/api/Store/GetStoreByUid?uId=${uId}`)
-                .then((response) => {
-                    setStoreId(response.id);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        } catch (error) {
-            console.log("Get Store By Uid error: " + error);
-        }
-    };
-
-    const reloadList = async () => {
-        try {
-            await axios
-                .get(`/api/Discount/ListDiscoutByStore?StoreId=${storeId}&CodeName=${discountFilter}&PageNumber=${pageNumber}&PageSize=${pageSize}`)
-                .then((res) => {
-                    console.log(res);
-                    setDiscountList(res.discounts);
-                    setTotalPages(res.metadata.totalPages);
-                })
-                .catch((error) => {
-                    toast.error("Có lỗi xảy ra!");
-                    console.log(error);
-                });
-        } catch (error) {
-            console.log("Discount error: " + error);
-        }
-
-    };
+  const GetStoreByUid = async () => {
     try {
       await axios
-        .get(`/api/Discount/ListDiscountByStore`, dataPost)
+        .get(`/api/Store/GetStoreByUid?uId=${uId}`)
+        .then((response) => {
+          setStoreId(response.id);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log("Get Store By Uid error: " + error);
+    }
+  };
+
+  const reloadList = async () => {
+    try {
+      await axios
+        .get(`/api/Discount/ListDiscoutByStore?StoreId=${storeId}&CodeName=${discountFilter}&PageNumber=${pageNumber}&PageSize=${pageSize}`)
         .then((res) => {
-          console.log(res);
-          setDiscountList(res);
+          setDiscountList(res.discounts);
+          setTotalPages(res.metadata.totalPages);
         })
         .catch((error) => {
           toast.error("Có lỗi xảy ra!");
@@ -85,14 +66,9 @@ const Discount = () => {
     GetStoreByUid();
   }, []);
 
-    useEffect(() => {
-        reloadList();
-    }, [storeId, pageNumber, discountFilter]);
-
-
   useEffect(() => {
     reloadList();
-  }, [pageNumber, discountFilter]);
+  }, [storeId, pageNumber, discountFilter]);
 
   const handlePageChange = (newPage) => {
     if (newPage >= 1 && newPage <= totalPages) {
@@ -111,146 +87,17 @@ const Discount = () => {
             <AddDiscount reload={reloadList} storeId={storeId}></AddDiscount>
 
 
-                        <div className="w-full shrink-0 gap-2 px-2 py-2 md:w-max">
-                            <div className="w-full md:w-72">
-                                <Input
-                                    label="Tìm kiếm"
-                                    icon={
-                                        <svg
-                                            width="20"
-                                            height="20"
-                                            viewBox="0 0 20 20"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path
-                                                d="M9.0625 15.625C12.6869 15.625 15.625 12.6869 15.625 9.0625C15.625 5.43813 12.6869 2.5 9.0625 2.5C5.43813 2.5 2.5 5.43813 2.5 9.0625C2.5 12.6869 5.43813 15.625 9.0625 15.625Z"
-                                                stroke="#191C1F"
-                                                strokeWidth="1.5"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                            />
-                                            <path
-                                                d="M13.7031 13.7031L17.5 17.5"
-                                                stroke="#191C1F"
-                                                strokeWidth="1.5"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                            />
-                                        </svg>
-                                    }
-                                    value={discountFilter}
-                                    onChange={(e) => setDiscountFilter(e.target.value)}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </CardHeader>
-                <CardBody className="px-0">
-                    <table className="w-full min-w-max table-auto text-center">
-                        <thead>
-                            <tr>
-                                {TABLE_HEAD.map((head) => (
-                                    <th
-                                        key={head}
-                                        className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
-                                    >
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal leading-none opacity-70"
-                                        >
-                                            {head}
-                                        </Typography>
-                                    </th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {discountList ? (
-                                discountList.map((discount, index) => (
-                                    <tr
-                                        key={discount.id}
-                                        className={
-                                            backgroundColors[index % backgroundColors.length]
-                                        }
-                                    >
-                                        <td>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal max-w-xs truncate"
-                                            >
-                                                {discount.code}
-                                            </Typography>
-                                        </td>
-                                        <td>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal max-w-xs truncate"
-                                            >
-                                                {discount.percent}
-                                            </Typography>
-                                        </td>
-                                        <td>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal max-w-xs truncate"
-                                            >
-                                                {discount.conditionPrice}
-                                            </Typography>
-                                        </td>
-                                        <td>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal"
-                                            >
-                                                {discount.quantity}
-                                            </Typography>
-                                        </td>
-                                        <td>
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal"
-                                            >
-                                                {discount.expired}
-                                            </Typography>
-                                        </td>
-                                        <td>
-                                            <div className="h-full flex justify-center items-center">
-                                                <UpdateDiscount
-                                                    reload={reloadList}
-                                                    discountData={discount}
-                                                    storeId={storeId}
-                                                ></UpdateDiscount>
-                                                <DeleteDiscount
-                                                    reload={reloadList}
-                                                    id={discount.id}
-                                                ></DeleteDiscount>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                ))
-                            ) : (
-                                <tr>
-                                    <td colSpan={TABLE_HEAD.length}>
-                                        <p>Chưa có ưu đãi nào!</p>
-                                    </td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </CardBody>
-                <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-                    <Button
-                        variant="outlined"
-                        size="sm"
-                        onClick={() => handlePageChange(pageNumber - 1)}
-
+            <div className="w-full shrink-0 gap-2 px-2 py-2 md:w-max">
+              <div className="w-full md:w-72">
+                <Input
+                  label="Tìm kiếm"
+                  icon={
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
                         d="M9.0625 15.625C12.6869 15.625 15.625 12.6869 15.625 9.0625C15.625 5.43813 12.6869 2.5 9.0625 2.5C5.43813 2.5 2.5 5.43813 2.5 9.0625C2.5 12.6869 5.43813 15.625 9.0625 15.625Z"
@@ -308,7 +155,7 @@ const Discount = () => {
                       <Typography
                         variant="small"
                         color="blue-gray"
-                        className="font-bold"
+                        className="font-normal max-w-xs truncate"
                       >
                         {discount.id}
                       </Typography>
@@ -320,6 +167,15 @@ const Discount = () => {
                         className="font-normal max-w-xs truncate"
                       >
                         {discount.code}
+                      </Typography>
+                    </td>
+                    <td>
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal max-w-xs truncate"
+                      >
+                        {discount.percent}
                       </Typography>
                     </td>
                     <td>
@@ -350,10 +206,29 @@ const Discount = () => {
                       </Typography>
                     </td>
                     <td>
+                      {discount.isExpired ?
+                        <Typography
+                          variant="h6"
+                          color="red"
+                          className="font-semibold"
+                        >
+                          HẾT HẠN
+                        </Typography>
+                        :
+                        <Typography
+                          variant="h6"
+                          color="green"
+                          className="font-semibold"
+                        >
+                          CÒN HẠN
+                        </Typography>
+                      }
+                    </td>
+                    <td>
                       <div className="h-full flex justify-center items-center">
                         <UpdateDiscount
                           reload={reloadList}
-                          foodData={discount}
+                          discountData={discount}
                           storeId={storeId}
                         ></UpdateDiscount>
                         <DeleteDiscount
@@ -402,8 +277,8 @@ const Discount = () => {
             Next
           </Button>
         </CardFooter>
-      </Card>
-    </div>
+      </Card >
+    </div >
   );
 };
 
