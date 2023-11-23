72c07cfab1609c68dd307f07eaf7316026ae7ba7
Lâm Quang Vinh <62162620+Vinhlam2512@users.noreply.github.com>
2023-11-24 06:55:47 +0700

    VinhLQ_24/11/2023_1
    Update combo + test

--- FFS/Test/AuthenticateControllerTest.cs
+++ FFS/Test/AuthenticateControllerTest.cs
@@ -26,74 +26,121 @@ namespace FFS.Test {
 
     public class AuthenticateControllerTest {
 
-       
+        private Mock<IOptionsMonitor<AppSetting>> mockOptionsMonitor;
+        private Mock<ApplicationDbContext> mockDbContext;
+        private Mock<IUserStore<ApplicationUser>> userStoreMock;
+        private Mock<IMapper> mockMapper;
+        private Mock<IEmailService> mockEmailService;
+        private Mock<DapperContext> mockDapperContext;
+        private Mock<IConfiguration> mockConfiguration;
+        private DapperContext dapperContext;
+        private Mock<UserManager<ApplicationUser>> mockUserManager;
+        private AuthRepository authRepository;
+        private AuthenticateController controller;
 
-
-
-        [Fact]
-        public void LoginByEmail_ReturnsOkResult_WhenCredentialsAreValid()
+        public AuthenticateControllerTest()
         {
-            //// Arrange
-            var mockOptionsMonitor = new Mock<IOptionsMonitor<AppSetting>>();
-            var mockDbContext = new Mock<ApplicationDbContext>();
-            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
-            var mockMapper = new Mock<IMapper>();
-            var mockEmailService = new Mock<IEmailService>();
-            var mockDapperContext = new Mock<DapperContext>();
+            // Initialize your mocks and other objects
+            mockOptionsMonitor = new Mock<IOptionsMonitor<AppSetting>>();
+            mockDbContext = new Mock<ApplicationDbContext>();
+            userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            mockMapper = new Mock<IMapper>();
+            mockEmailService = new Mock<IEmailService>();
+            mockDapperContext = new Mock<DapperContext>();
+            mockConfiguration = new Mock<IConfiguration>();
 
             var appSettings = new AppSetting();
             mockOptionsMonitor.Setup(m => m.CurrentValue).Returns(appSettings);
 
-            var mockConfiguration = new Mock<IConfiguration>();
-
             mockConfiguration.SetupGet(x => x[It.Is<string>(s => s == "ConnectionStrings")]).Returns("DefaultConnection");
-            var dapperContext = new DapperContext(mockConfiguration.Object);
+            mockConfiguration.SetupGet(x => x[It.Is<string>(s => s == "JWT")]).Returns("SecretKey");
 
-            var mockUserManager = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
-            var authRepository = new AuthRepository(mockUserManager.Object, mockOptionsMonitor.Object, mockEmailService.Object, mockDbContext.Object, dapperContext);
+            dapperContext = new DapperContext(mockConfiguration.Object);
 
+            mockUserManager = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
+            authRepository = new AuthRepository(mockUserManager.Object, mockOptionsMonitor.Object, mockEmailService.Object, mockDbContext.Object, dapperContext);
 
-            var controller = new AuthenticateController(
-                mockDbContext.Object
-                , mockUserManager.Object
-                , authRepository
-                , mockMapper.Object
-                , mockEmailService.Object
+            controller = new AuthenticateController(
+                mockDbContext.Object,
+                mockUserManager.Object,
+                authRepository,
+                mockMapper.Object,
+                mockEmailService.Object
             );
-            var loginDto = new LoginDTO { Email = "test@example.com", Password = "password" };
+        }
 
+
+
+
+        [Fact]
+        public async void LoginByEmail_ReturnsBadRequest_WhenWrongPass()
+        {
+            var loginDto = new LoginDTO { Email = "tuanht22042001@gmail.com", Password = "123@123aA" };
             // Set up mock behavior for the dependencies
-            mockUserManager.Setup(m => m.FindByEmailAsync(It.IsAny<string>())).Returns(Task.FromResult<ApplicationUser>(null));
-            mockUserManager.Setup(m => m.CheckPasswordAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).Returns(Task.FromResult(false));
-            mockUserManager.Setup(m => m.GetRolesAsync(It.IsAny<ApplicationUser>())).Returns(Task.FromResult<IList<string>>(new List<string>() { "Admin" }));
+            ApplicationUser capturedUser = new ApplicationUser();
+            mockUserManager
+                .Setup(m => m.FindByEmailAsync(loginDto.Email))
+                .ReturnsAsync(capturedUser);
+            mockUserManager
+                 .Setup(m => m.CheckPasswordAsync(capturedUser, loginDto.Password))
+                 .Returns(Task.FromResult(false));
 
             // Act
-
-            var result = controller.LoginByEmail(loginDto);
+            var result = await controller.LoginByEmail(loginDto);
 
             // Assert
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
             Assert.Equal(400, badRequestResult.StatusCode);
-
+            Assert.Equal("Mật khẩu không đúng !", badRequestResult.Value);
         }
 
-        //[Fact]
-        //public void LoginByEmail_ReturnsUnauthorizedResult_WhenCredentialsAreInvalid()
-        //{
-        //    // Arrange
-        //    var mockRepo = new Mock<IAuthRepository>();
-        //    var controller = new YourController(mockRepo.Object);
-        //    var loginDto = new LoginDTO { Email = "test@example.com", Password = "wrongpassword" };
+        [Fact]
+        public async void LoginByEmail_ReturnsBadRequest_WhenDontFillInfor()
+        {
+            var loginDto = new LoginDTO { Email = "", Password = "" };
+            // Set up mock behavior for the dependencies
+            ApplicationUser capturedUser = new ApplicationUser();
+            mockUserManager
+                .Setup(m => m.FindByEmailAsync(loginDto.Email))
+                .ReturnsAsync(capturedUser);
+            mockUserManager
+                 .Setup(m => m.CheckPasswordAsync(capturedUser, loginDto.Password))
+                 .Returns(Task.FromResult(false));
 
-        //    mockRepo.Setup(repo => repo.Login(loginDto.Email, loginDto.Password))
-        //        .Returns((UserClient)null); // return null to simulate invalid credentials
+            // Act
+            var result = await controller.LoginByEmail(loginDto);
 
-        //    // Act
-        //    var result = controller.LoginByEmail(loginDto);
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal(400, badRequestResult.StatusCode);
+        }
 
-        //    // Assert
-        //    Assert.IsType<UnauthorizedResult>(result);
-        //}
+        
+        [Fact]
+        public async void LoginByEmail_ReturnsOk()
+        {
+            var loginDto = new LoginDTO { Email = "tuanht22042001 @gmail.com ", Password = "123@123aA" };
+            // Set up mock behavior for the dependencies
+            ApplicationUser capturedUser = new ApplicationUser
+            {
+                UserName = "YourUsername",
+                Email = loginDto.Email,
+                Id = "YourUserId" // Replace with the actual user id
+            };
+            mockUserManager
+                .Setup(m => m.FindByEmailAsync(loginDto.Email))
+                .ReturnsAsync(capturedUser);
+            mockUserManager
+                 .Setup(m => m.CheckPasswordAsync(capturedUser, loginDto.Password))
+                 .Returns(Task.FromResult(true));
+
+            // Act
+            var result = await controller.LoginByEmail(loginDto);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal(400, badRequestResult.StatusCode);
+        }
 
 
     }
